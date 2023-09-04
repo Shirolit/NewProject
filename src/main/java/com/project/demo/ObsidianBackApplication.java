623031282b434dea4164decadd81c1092f0e8f1a
Shiro <lokotkov854@mail.ru>
2023-09-04 20:03:27 +0600

    Голое приложение

---
+++ src/main/java/com/project/demo/ObsidianBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsidianBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsidianBackApplication.class, args);
+	}
+
+}
